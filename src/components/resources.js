48dacb5fd427eef4755450487a2f2d790b5372f9
Stephen Mayeux <stephenmayeux@gmail.com>
2016-10-06 11:09:31 -0400

    protected resource

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <h1>Super Secret Special Recipe</h1>
+      <ul>
+        <li>1 cup sugar</li>
+        <li>1 cup salt</li>
+        <li>1 cup gochujang</li>
+      </ul>
+    </div>
+  );
+}
