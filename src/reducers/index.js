0ac6c3d90086e880a64bfc0dcadde3946e1a42f2
Stephen Mayeux <stephenmayeux@gmail.com>
2016-10-06 11:30:39 -0400

    wire up action creator to button

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import authenticationReducer froom './authentication';
+import authenticationReducer from './authentication';
 
 const rootReducer = combineReducers({
   authenticated: authenticationReducer
